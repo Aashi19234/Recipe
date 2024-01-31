eab04eb6f8386535f311f56744aa6934dab75d7e
Aashi19234 <aasthaj375@gmail.com>
2024-02-01 01:13:06 +0530

    done

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const searchbox=document.querySelector('.searchbox');
 const searchbtn=document.querySelector('.search-btn');
 const recipecontainer=document.querySelector('.recipe-container');
-
+const recipedetailscontent=document.querySelector('.recipe-details-content');
+const recipeclosebtn=document.querySelector('.recipe-close-btn');
 const fetchrecipes= async (query)=>{
-    recipecontainer.innerHTML="Fetching recipes"
-    const data= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    recipecontainer.innerHTML="<h2>Fetching recipes...</h2>"
+    try {
+        const data= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
 
     const response= await data.json();
     recipecontainer.innerHTML="";
@@ -16,21 +18,83 @@ const fetchrecipes= async (query)=>{
         recipediv.innerHTML=`
         <img src="${meal.strMealThumb}">
         <h3>${meal.strMeal}</h3>
-        <p>${meal.strArea}</p>
-        <p>${meal.strCategory}</p>`
+        <p><span>${meal.strArea}</span> Dish</p>
+        <p>Belongs to <span>${meal.strCategory}</span> category</p>`
+        const button= document.createElement('button');
+        button.textContent="View Recipe";
+        recipediv.appendChild(button);
+        button.addEventListener('click', ()=>{
+            //pop-up window for recipe
+            openRecipePopup(meal);
+        });
 
         recipecontainer.appendChild(recipediv);// container mei jo element create kia h usko append krdia
 
 
         
     });
+    }
+     catch (error) {
+        recipecontainer.innerHTML="<h2>Error in fetching recipes...</h2>"
+        
+    }
+    
+    
     //console.log(response.meals[0]);// jo pehla meal tha vo access ho gya
 
 }
+const fetchIngredients=(meal)=>{
+    //console.log(meal);
+    let ingredientslist="";
+    for(let i=1;i<=20;i++){
+        const ingredient = meal[`strIngredient${i}`];
+        if(ingredient){
+            const measure=meal[`strMeasure${i}`];
+            ingredientslist+=`<li>${measure} ${ingredient}</li>`
+
+
+        }
+        else{
+            break;
+
+        }
+    }
+    return ingredientslist;
+
+
+}
+
+
+const openRecipePopup=(meal)=>{
+    recipedetailscontent.innerHTML=`
+    <h2 class="recipename">${meal.strMeal}</h2>
+    <h3>Ingredients:</h3>
+    <ul class="ingredientlist">${fetchIngredients(meal)}</ul>
+    <div  class="recipeinstructions">
+    <h3>Instructions:</h3>
+    <p>${meal.strInstructions}</p>
+    </div>
+    
+    
+    
+    `
+    
+    recipedetailscontent.parentElement.style.display="block";
+
+}
+recipeclosebtn.addEventListener('click', ()=>{
+    recipedetailscontent.parentElement.style.display="none";
+});
+
 
 searchbtn.addEventListener('click', (e)=>{
     e.preventDefault();//autosubmit ni hoega , page refresh ni hga button click krne p
 const searchinput=searchbox.value.trim();
+if(!searchinput){
+    recipecontainer.innerHTML=`<h2>Type the meal in the search box.</h2>`;
+    return;
+}
    fetchrecipes(searchinput);
 });
 
+  // ``-> template literals
